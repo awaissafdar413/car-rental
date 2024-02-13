7be1fd425fa08ad5e82e5cb6de21e33b5725183a
awaissafdar413 <159916432+awaissafdar413@users.noreply.github.com>
2024-02-13 19:14:37 +0500

    sx

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,30 +1,45 @@
 @extends('layout.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Forget Password:</h1>
-        </div>
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
-            <div class="forget_password">
-                <form method="POST" action="{{ route('password.email') }}">
-                    @csrf
+<!-- header close -->
+<!-- content begin -->
+<div class="no-bottom no-top" id="content">
+    @if (session('status'))
+    <div class="mb-4 font-medium text-sm text-green-600">
+        {{ session('status') }}
+    </div>
+    @endif
+    <div id="top"></div>
+    <section id="section-hero" aria-label="section" class="jarallax">
+        <img src="images/background/2.jpg" class="jarallax-img" alt="">
+        <div class="v-center">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 offset-lg-4">
+                        <div class="padding40 rounded-3 shadow-soft" data-bgcolor="#09060670">
+                            <h4>Forget Password</h4>
+                            <div class="spacer-10"></div>
+                            <form method="POST" action="{{ route('password.email') }}">
+                                @csrf
 
-                    <div class="block">
-                        <x-label for="email" value="{{ __('Email') }}" />
-                        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                    </div>
+                                <div class="block">
+                                    <x-label for="email" value="{{ __('Email') }}" />
+                                    <x-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                        :value="old('email')" required autofocus autocomplete="username" />
+                                </div>
 
-                    <div class="flex items-center justify-end mt-4">
-                        <x-button>
-                            {{ __('Email Password Reset Link') }}
-                        </x-button>
+                                <div class="flex items-center justify-end mt-4">
+                                    <x-button>
+                                        {{ __('Email Password Reset Link') }}
+                                    </x-button>
+                                </div>
+                            </form>
+
+                        </div>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
-        <div class="col-md-2"></div>
-    </div>
+    </section>
 </div>
+<!-- content close -->
 @endsection
